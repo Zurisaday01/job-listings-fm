c941ebf4e31e658f6619c452d036ffc9ed6159d7
Zurisaday Espadas <zuryespadas@Zurisadays-MacBook-Air.local>
2024-04-08 11:26:26 -0600

    Fix Responsive Card Status

--- public/images/Images.ts
+++
@@ -1,7 +0,0 @@
-const IMAGES = {
-	removeIcon: new URL('./icon-remove.svg', import.meta.url).href,
-	bgHeaderDesktop: new URL('./bg-header-desktop.svg', import.meta.url).href,
-	bgHeaderMobile: new URL('./bg-header-mobile.svg', import.meta.url).href,
-};
-
-export default IMAGES;
